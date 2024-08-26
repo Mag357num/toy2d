4f56fa003c729b710945d973b6bfa603e57591b2
magnum <>
2024-08-26 17:40:48 +0800

    适合我在windows上使用的cmakelist

--- cmake/CopyFiles.cmake
+++ cmake/CopyFiles.cmake
@@ -1,12 +1,9 @@
 macro(CopyDLL target_name)
     if (WIN32)
+        # If you have selected SDL2 component when installed Vulkan SDK, the command as follows will work
         add_custom_command(
             TARGET ${target_name} POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy ${SDL2_ROOT}/lib/x64/SDL2.dll $<TARGET_FILE_DIR:${target_name}>)
-        # If you have selected SDL2 component when installed Vulkan SDK, the command as follows will work
-        # add_custom_command(
-        #     TARGET ${target_name} POST_BUILD
-        #     COMMAND ${CMAKE_COMMAND} -E copy ${SDL2_BIN_DIR}/SDL2.dll $<TARGET_FILE_DIR:${target_name}>)
+            COMMAND ${CMAKE_COMMAND} -E copy ${SDL2_BIN_DIR}/SDL2.dll $<TARGET_FILE_DIR:${target_name}>)
     endif()
 endmacro(CopyDLL)
 
